9fc164b0b1376d6573611e8ae67def8964365ef8
Matthew Collier <mcollier@digitalbazaar.com>
2020-12-23 12:46:42 -0500

    Apply updates.

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # ed25519-dalek
+
+### Install nj-cli
+```
+cargo install nj-cli
+```
+
+### Build with AVX/SIMD optimizations
+```
+RUSTFLAGS="-C target_cpu=native" nj-cli build --release
+```
--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,53 @@ use ed25519_dalek::{Keypair, PublicKey, Signer, Verifier, Signature};
 use base58::{FromBase58, ToBase58};
 use node_bindgen::derive::node_bindgen;
 use node_bindgen::core::buffer::ArrayBuffer;
-use node_bindgen::core::NjError;
+// use node_bindgen::core::{NjError};
+// use node_bindgen::core::val::JsObject;
+// use base64;
+
+// #[node_bindgen]
+// pub fn sign(private_key_bytes: &[u8], message: &[u8]) -> Result<String,NjError> {
+//   let keypair: Keypair = Keypair::from_bytes(private_key_bytes).unwrap();
+//   ArrayBuffer::new(keypair.sign(message).to_bytes().to_vec());
+//   Ok("foo".to_string())
+// }
+
+// #[node_bindgen]
+// pub fn sign(private_key_bytes: String, message: String) -> ArrayBuffer {
+//     // let k = private_key_bytes.as_value::<String>().unwrap();
+//     let key_bytes = private_key_bytes.from_base58().unwrap();
+//     // let m = message.as_value::<String>().unwrap();
+//     let key_u8: &[u8] = &key_bytes;
+//     let keypair: Keypair = Keypair::from_bytes(key_u8).unwrap();
+//     ArrayBuffer::new(keypair.sign(message.as_bytes()).to_bytes().to_vec())
+// }
 
 #[node_bindgen]
-pub fn sign(private_key_bytes: &[u8], message: &[u8]) -> Result<String,NjError> {
-  let keypair: Keypair = Keypair::from_bytes(private_key_bytes).unwrap();
-  ArrayBuffer::new(keypair.sign(message).to_bytes().to_vec());
-  Ok("foo".to_string())
+pub fn sign2(private_key_bytes: String, message: &[u8]) -> ArrayBuffer {
+    let key_bytes = private_key_bytes.from_base58().unwrap();
+    let key_u8: &[u8] = &key_bytes;
+    let keypair: Keypair = Keypair::from_bytes(key_u8).unwrap();
+    ArrayBuffer::new(keypair.sign(message).to_bytes().to_vec())
+}
+
+#[node_bindgen]
+pub fn sign3(private_key_bytes: String, message: &[u8]) -> String {
+    let key_bytes = private_key_bytes.from_base58().unwrap();
+    let key_u8: &[u8] = &key_bytes;
+    let keypair: Keypair = Keypair::from_bytes(key_u8).unwrap();
+    // print!("3333333 message length {}\n", message.len());
+    base64::encode(keypair.sign(message).to_bytes())
+}
+
+#[node_bindgen]
+pub fn sign4(private_key_bytes: String, message: String) -> String {
+    let key_bytes = private_key_bytes.from_base58().unwrap();
+    let key_u8: &[u8] = &key_bytes;
+    let keypair: Keypair = Keypair::from_bytes(key_u8).unwrap();
+    let message_bytes = message.as_bytes();
+    // print!("4444444 message length {}\n'", message.len());
+    // print!("bytes {:?}\n", message_bytes);
+    base64::encode(keypair.sign(message_bytes).to_bytes())
 }
 
 // pub fn sign_x(private_key_bytes: &[u8], message: &[u8], ) -> [u8; 64] {
